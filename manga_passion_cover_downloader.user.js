72c67adc0513dbfd9dc058a9a991514e0d5ce64b
Nino Singer <Ninelpienel@gmail.com>
2026-02-13 03:28:52 +0100

    Update manga_passion_cover_downloader.user.js

--- manga_passion_cover_downloader.user.js
+++ manga_passion_cover_downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Manga Passion Cover Downloader
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  Download high-resolution covers from Manga-Passion.de
 // @author       You
 // @match        https://www.manga-passion.de/editions/*
