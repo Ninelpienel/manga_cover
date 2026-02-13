c2e7b2b25af54fa0f20b206dd78586a856ba7df9
Nino Singer <Ninelpienel@gmail.com>
2026-02-13 03:17:47 +0100

    Update manga_passion_cover_downloader.user.js

--- manga_passion_cover_downloader.user.js
+++ manga_passion_cover_downloader.user.js
@@ -6,7 +6,7 @@
 // @author       You
 // @match        https://www.manga-passion.de/editions/*
 // @grant        none
-// @downloadURL  https://github.com/HarlemShakespeare/YOUR_REPO_NAME/raw/master/YOUR_USERSCRIPT.user.js
+// @downloadURL  https://github.com/Ninelpienel/manga_cover/raw/master/manga_passion_cover_downloader.user.js
 // ==/UserScript==
 
 (function() {
